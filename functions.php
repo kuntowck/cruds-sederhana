384a947f7dfa45cc0c5956ec3d20bae491a9cc9c
kuntowck <kunto312002@gmail.com>
2024-03-17 15:36:48 +0700

    Menambahkan folder img dan berhasil menyambungkan database, serta query

--- functions.php
+++ functions.php
@@ -1,1 +1,18 @@
 <?php
+function koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'warkop');
+}
+
+function query($query)
+{
+  $db = koneksi();
+  $result = mysqli_query($db, $query);
+  $rows = [];
+
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+
+  return $rows  ;
+}
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+require 'functions.php';
+
+$warkop = query("SELECT * FROM menu");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,13 +16,32 @@
 <body>
   <h3>Daftar Warkop</h3>
 
-  <table border="1" cell-spacing="0" cell-padding="10">
+  <table border="1" cellspacing="0" cellpadding="10">
     <tr>
       <th>#</th>
       <th>Nama</th>
       <th>Alamat</th>
+      <th>Makanan</th>
+      <th>Minuman</th>
+      <th>Gambar</th>
+      <th>Aksi</th>
     </tr>
-    
+
+    <?php $i = 1;
+    foreach ($warkop as $w) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><?= $w['tempat']; ?></td>
+        <td><?= $w['alamat']; ?></td>
+        <td><?= $w['makanan']; ?></td>
+        <td><?= $w['minuman']; ?></td>
+        <td><img width="100" src="img/<?= $w['gambar']; ?>" alt=""></td>
+        <td>
+          <a href="">Ubah</a> | 
+          <a href="">Delete</a>
+        </td>
+      </tr>
+    <?php endforeach; ?>
   </table>
 </body>
 
