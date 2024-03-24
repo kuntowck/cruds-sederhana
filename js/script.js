0653ddc0410f954073d64c3778a053fb9bbd0e37
kuntowck <kunto312002@gmail.com>
2024-03-24 12:35:01 +0700

    fitur live search menggunakan fetch

--- js/script.js
+++ js/script.js
@@ -5,20 +5,8 @@ const container = document.querySelector('.container');
 tombolCari.style.display = 'none';
 
 keyword.addEventListener('keyup', function(){
-    ajax - xmlhttprequest
-    const xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState == 4 && xhr.status == 200) {
-            container.innerHTML = xhr.responseText;
-        }
-    }
-
-    xhr.open('get', 'ajax/ajax.php?keyword=' + keyword.value);
-    xhr.send();
-
-    // // fetch()
-    // fetch('ajax/ajax.php?keyword=' + keyword.value)
-    //     .then(response => response.text())
-    //     .then(response => container.innerHTML = response);
+    // fetch()
+    fetch('ajax/ajax.php?keyword=' + keyword.value)
+        .then(response => response.text())
+        .then(response => container.innerHTML = response);
 });
